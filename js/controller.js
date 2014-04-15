d9b126159241418d7650455da68a2ae532ec58c6
Edwin L <eleong@sfu.ca>
2014-04-15 03:13:08 -0700

    fixed space bug in city name

--- js/controller.js
+++ js/controller.js
@@ -6,11 +6,12 @@ var weatherApp = angular.module('weatherApp', []);
         $scope.enteredCity = '';
         $scope.fiveDayForecast = [];
         $scope.getWeather = function() {
-            $scope.city = $scope.enteredCity;
+            // Ensure strange characters and spaces are removed for the YQL
+            $scope.city = $scope.enteredCity.replace(/\s+/g, '');;
             $http.get('https://query.yahooapis.com/v1/public/yql?q=SELECT%20*%20FROM%20weather.bylocation%20WHERE%20location%3D%22' + $scope.city + '%22&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback=').success(function(data){
             $scope.fiveDayForecast = data.query.results.weather.rss.channel.item.forecast;
             $scope.title = data.query.results.weather.rss.channel.title;
-            //clear the form
+            // clear the form
             $scope.enteredCity = '';
             })
         }
